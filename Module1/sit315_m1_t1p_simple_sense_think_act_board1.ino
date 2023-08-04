345853e9f73d5efbd3a33ef3ae6b4132343502dd
Andrew Belcher <belcher.w.andrew@gmail.com>
2023-08-04 11:28:22 +0800

    First commit

---
+++ Module1/sit315_m1_t1p_simple_sense_think_act_board1.ino
@@ -0,0 +1,56 @@
+// Set the PINS for the button and LED
+const uint8_t SENSOR_PIN = 7;
+const uint8_t LED_PIN = 13;
+
+// Create setup function
+void setup()
+{
+  // Start the serial monitor
+  Serial.begin(9600);
+}
+
+void loop()
+{
+	long duration, cm;
+  
+  	// The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  	// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  	pinMode(SENSOR_PIN, OUTPUT);
+    digitalWrite(SENSOR_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(SENSOR_PIN, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(SENSOR_PIN, LOW);
+  
+  	// The same pin is used to read the signal from the PING))): a HIGH pulse
+    // whose duration is the time (in microseconds) from the sending of the ping
+    // to the reception of its echo off of an object.
+    pinMode(SENSOR_PIN, INPUT);
+    duration = pulseIn(SENSOR_PIN, HIGH);
+  
+  	cm = microsecondsToCentimeters(duration);
+  
+  	Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+
+  	if(cm < 100)
+    {
+      digitalWrite(LED_PIN, HIGH);
+    }
+  	else
+    {
+      digitalWrite(LED_PIN, LOW);
+    }
+  
+  
+  	delay(100);
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  	// The speed of sound is 340 m/s or 29 microseconds per centimeter.
+    // The ping travels out and back, so to find the distance of the object we
+    // take half of the distance travelled.
+ 	return microseconds / 29 / 2; 
+}
+  
